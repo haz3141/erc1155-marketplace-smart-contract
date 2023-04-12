e100df699a8ef66df3595c05567ae67de93b0feb
haz3141 <38766403+haz3141@users.noreply.github.com>
2023-04-12 01:29:01 -0400

    Initialized contract

---
+++ NFTeesOneMarketplace.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+
+contract NFTeesOneMarketplace is ERC1155 {
+    constructor() ERC1155("") {}
+}
