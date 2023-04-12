dd06a2ad3958c095bf3f55adec5ceed47d663996
haz3141 <38766403+haz3141@users.noreply.github.com>
2023-04-12 02:35:07 -0400

    Implemented access control

--- ERC1155Marketplace.sol
+++ ERC1155Marketplace.sol
@@ -2,7 +2,21 @@
 pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract ERC1155Marketplace is ERC1155 {
-    constructor() ERC1155("") {}
+contract ERC1155Marketplace is ERC1155, AccessControl {
+    constructor() ERC1155("") {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC1155, AccessControl)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
 }
